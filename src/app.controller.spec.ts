332cd4ac21c1ccb7336c46c2ac3c67c2a0f3e1c7
The-BigMan-tech <saddlebox9@gmail.com>
2025-01-04 20:25:23 +0100

    Almost done with unit tests

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -2,21 +2,42 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+
 describe('AppController', () => {
   let appController: AppController;
+  let appService:AppService
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [
+        {
+          provide:AppService,
+          useValue:{
+            getHello:jest.fn().mockReturnValue('Hello World!')
+          }
+        }
+      ],
     }).compile();
 
     appController = app.get<AppController>(AppController);
+    appService = app.get<AppService>(AppService);
   });
 
-  describe('root', () => {
+  describe('root operations:', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+      expect(appService.getHello()).toBe('Hello World!');
     });
+    it('should mock the service',()=>{
+      //jest.spyOn(appService,'getHello').mockReturnValue('hello')
+      expect(appController.getHello()).toBe('hello')
+    })
+    it('should return a number',async()=>{
+      const result:number = await appController.getTwo()
+      expect(result).toBe(10)
+    })
+    it('should return the average of the given numbers',()=>{
+      expect(appController.getAvg(1,2,3)).toBe(2)
+    })
   });
 });
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,19 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  public getHello(): string {
     return this.appService.getHello();
+  }
+  @Get('two')
+  public async getTwo():Promise<number> {
+    return 10
+  }
+  @Get('average')
+  public getAvg(...numbers:number[]):number {
+      let sum:number = 0
+      for (let number of numbers) {
+        sum += number
+      }
+      return sum/numbers.length
   }
 }
